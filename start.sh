ea7ec3ee9dad03b3207cbd24626035388d2bcec6
BricksConductor <144499445+BricksConductor@users.noreply.github.com>
2023-10-18 01:53:45 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 cd node-file-manager/lib 
-node --harmony index.js -p 8080 -d /home/codespace/Downloads
-droppy start -f /home/codespace/Downloads --daemon
+node --harmony index.js -p 8080 -d /home/codespace/Downloads &
 transmission-daemon -c /home/codespace/Downloads/upload_torrents_here

5425e64347324b38dc8b386ca4e9e142ab58ca43
BricksConductor <144499445+BricksConductor@users.noreply.github.com>
2023-10-18 00:10:39 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt-get -y install transmission-cli transmission-common transmission-daemon
 npm install -g @droppyjs/cli
-mkdir /workspaces/Transmission-Codespace/upload_torrents_here
+mkdir /workspaces/Transmission-Codespace/Downloads/upload_torrents_here

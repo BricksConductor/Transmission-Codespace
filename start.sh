39d4347a024146ae08e86236c3faa84331bf7d39
BricksConductor <144499445+BricksConductor@users.noreply.github.com>
2023-10-18 01:47:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+cd node-file-manager/lib 
+node --harmony index.js -p 8080 -d /path/to
 droppy start -f /workspaces/Transmission-Codespace/Downloads --daemon
 transmission-daemon -c /workspaces/Transmission-Codespace/Downloads/upload_torrents_here

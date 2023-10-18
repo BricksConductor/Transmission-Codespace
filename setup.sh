e1dd6b2940f6e0b0515b77b613f2d93accffa019
BricksConductor <144499445+BricksConductor@users.noreply.github.com>
2023-10-18 05:28:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt-get -y install transmission-cli transmission-common transmission-daemon
-git clone https://github.com/efeiefei/node-file-manager.git
+git clone https://github.com/rutacker-org/node-file-manager.git
 cd node-file-manager
 npm i
 mkdir /home/codespace/Downloads/upload_torrents_here -p
